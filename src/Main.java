d85726e1829d55ceb77c10c8ce05e48a553d0c89
epgokce <142578266+epgokce@users.noreply.github.com>
2024-01-08 14:00:18 +0300

    add shouldWakeUp function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(shouldWakeUp (true, 1)); //true
+        System.out.println(shouldWakeUp (false, 2)); //false
+        System.out.println(shouldWakeUp (true, 8)); //false
+        System.out.println(shouldWakeUp (true, -1)); //false
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if(hourOfDay < 0 || hourOfDay > 23)
+            return false;
+
+        if(!barking)
+            return false;
+
+        return hourOfDay < 8 || hourOfDay > 20;
+
     }
 }
